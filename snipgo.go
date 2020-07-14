ded55cfae9c8365358d34ac98f74d24b4c309109
nwillc <nwillc@gmail.com>
2020-07-14 13:57:48 -0400

    refactoring files

--- snipgo.go
+++ snipgo.go
@@ -5,24 +5,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sort"
 )
-
-const preferences = ".snippets.json"
-
-type Preferences struct {
-	DefaultFile string `json:"defaultFile"`
-}
-
-type Snippet struct {
-	Category string `json:"category"`
-	Title string `json:"title"`
-	Body string `json:"body"`
-}
 
 func main() {
 	fmt.Println("Start")
 	home, err := os.UserHomeDir()
-	prefs := fmt.Sprintf("%s/%s", home, preferences)
+	prefs := fmt.Sprintf("%s/%s", home, preferencesFile)
 	jsonFile, err := os.Open(prefs)
 	if err != nil {
 		fmt.Println(err)
@@ -58,7 +47,8 @@ func main() {
 		os.Exit(-2)
 	}
 
+	sort.Sort(ByCategory(snippets))
 	for _, snippet := range snippets {
-		fmt.Printf("%s: %s\n%s\n", snippet.Category, snippet.Title, snippet.Body)
+		fmt.Println(snippet.String())
 	}
 }
--- snippets.go
+++ snippets.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const preferencesFile = ".snippets.json"
+
+type Preferences struct {
+	DefaultFile string `json:"defaultFile"`
+}
+
+type Snippet struct {
+	Category string `json:"category"`
+	Title    string `json:"title"`
+	Body     string `json:"body"`
+}
+
+func (s Snippet) String() string { return fmt.Sprintf("%s: %s", s.Category, s.Title) }
+
+type ByCategory []Snippet
+
+func (s ByCategory) Len() int      { return len(s) }
+func (s ByCategory) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s ByCategory) Less(i, j int) bool {
+	return strings.ToLower(s[i].Category) < strings.ToLower(s[j].Category)
+}
