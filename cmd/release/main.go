434f2d40e6d547f6cf9184fe9106b799415158f1
nwillc <nwillc@gmail.com>
2020-09-12 01:18:05 -0400

    fixing imports

--- cmd/release/main.go
+++ cmd/release/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
-	"github.com/zyedidia/micro/tools/semver"
 	"io/ioutil"
 	"log"
 	"os"
